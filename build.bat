9685d1ae6fc342d3d89b1783d11cf3cc52b64074
Vincent Tavernier <vince.tavernier@gmail.com>
2019-05-09 11:45:24 +0200

    Switch to Travis CI for Windows integration

---
+++ build.bat
@@ -0,0 +1,11 @@
+REM Install cargo make if needed
+cargo make -V
+
+IF %ERRORLEVEL% NEQ 0 (
+    wget -O cargo-make.zip "https://github.com/sagiegurari/cargo-make/releases/download/0.18.0/cargo-make-v0.18.0-x86_64-pc-windows-msvc.zip"
+    7z x cargo-make.zip
+    move cargo-make.exe %USERPROFILE%\.cargo\bin
+)
+
+REM Build
+cargo make workspace-ci-flow --no-workspace
