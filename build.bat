f6a7de38dd086be872bedaf3b17c28c463ba87fa
Vincent Tavernier <vince.tavernier@gmail.com>
2019-05-22 11:18:39 +0200

    Extract cargo-make.exe to cargo bin dir

--- build.bat
+++ build.bat
@@ -3,8 +3,7 @@ cargo make -V
 
 IF %ERRORLEVEL% NEQ 0 (
     wget -qO cargo-make.zip "https://github.com/sagiegurari/cargo-make/releases/download/0.18.0/cargo-make-v0.18.0-x86_64-pc-windows-msvc.zip"
-    7z x cargo-make.zip
-    move cargo-make.exe %USERPROFILE%\.cargo\bin
+    7z x cargo-make.zip -o%USERPROFILE%\.cargo\bin cargo-make.exe -y
 )
 
 REM Build
