a8e22264ffa27d484119f2aabe19653844818d07
Vincent Tavernier <vince.tavernier@gmail.com>
2020-01-12 17:57:15 +0100

    Fix #14e61e

--- efiboot/src/cli/read.rs
+++ efiboot/src/cli/read.rs
@@ -3,7 +3,7 @@ use efivar::VarManager;
 use itertools::Itertools;
 
 pub fn run(reader: Box<dyn VarManager>, name: &str, as_string: bool) {
-    let mut buf = vec![0u8; 2];
+    let mut buf = vec![0u8; 512];
 
     match reader.read(&name, &mut buf[..]) {
         Ok((size, attr)) => {
