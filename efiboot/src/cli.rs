ed60566b5dc42f923815ae5acafd16859898d868
iTrooz <hey@itrooz.fr>
2023-07-29 01:05:08 +0200

    add `efiboot import`

--- efiboot/src/cli.rs
+++ efiboot/src/cli.rs
@@ -1,6 +1,7 @@
 mod boot;
 mod delete;
 mod dump;
+mod import;
 mod list;
 mod read;
 
@@ -8,5 +9,6 @@ pub use self::boot::get_entries as get_boot_entries;
 pub use self::boot::get_order as get_boot_order;
 pub use self::delete::run as delete;
 pub use self::dump::run as dump;
+pub use self::import::run as import;
 pub use self::list::run as list;
 pub use self::read::run as read;
---
+++ efiboot/src/cli/import.rs
@@ -0,0 +1,49 @@
+use std::{fs::File, io::Read, path::Path};
+
+use uuid::Uuid;
+
+use byteorder::{LittleEndian, ReadBytesExt};
+
+use efivar::{
+    efi::{VariableFlags, VariableName, VariableVendor},
+    VarManager,
+};
+
+fn read_var_from_file(input_path: &Path) -> Result<(VariableFlags, Vec<u8>), std::io::Error> {
+    let mut file = File::open(input_path)?;
+
+    let flags = VariableFlags::from_bits(file.read_u32::<LittleEndian>()?).unwrap();
+    let mut data = Vec::new();
+    file.read_to_end(&mut data)?;
+
+    Ok((flags, data))
+}
+
+pub fn run(
+    mut manager: Box<dyn VarManager>,
+    input_path: &Path,
+    name: &str,
+    namespace: Option<Uuid>,
+) {
+    let var = VariableName::new_with_vendor(
+        name,
+        namespace.map_or(VariableVendor::Efi, VariableVendor::Custom),
+    );
+
+    let (flags, data) = match read_var_from_file(input_path) {
+        Ok(inner) => inner,
+        Err(err) => {
+            eprintln!(
+                "Failed to read variable from file {}: {}",
+                input_path.display(),
+                err
+            );
+            return;
+        }
+    };
+
+    match manager.write(&var, flags, &data) {
+        Ok(()) => println!("Imported variable {} with success", var),
+        Err(err) => eprintln!("Failed to write variable {}: {}", var, err),
+    }
+}
