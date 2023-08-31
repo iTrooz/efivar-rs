3af520a17a31c9c1aa04924039ec11913cd3647c
iTrooz <hey@itrooz.fr>
2023-08-31 18:08:35 +0200

    test: add tests for read_nt_utf16_string()

--- efivar/src/utils.rs
+++ efivar/src/utils.rs
@@ -17,12 +17,57 @@ pub fn read_nt_utf16_string(cursor: &mut &[u8]) -> Result<String, StringParseErr
             .read_u16::<LittleEndian>()
             .map_err(StringParseError::Read)?
         {
-            0 => {
+            0x0000 => {
                 return String::from_utf16(&vec).map_err(StringParseError::Parse);
             }
             chr => {
                 vec.push(chr);
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn read_string() -> Result<(), StringParseError> {
+        let data: Vec<u8> = vec![b'a', 0x00, b'b', 0x00, b'c', 0x00, 0x00, 0x00, 0xFF]; // abc + null termination + a byte
+        let data_slice = &mut &data[..];
+
+        // verify extracted string is right
+        assert_eq!(read_nt_utf16_string(data_slice)?, "abc");
+
+        // verify the byte is left
+        assert_eq!(data_slice, &vec![0xFF]);
+        Ok(())
+    }
+
+    #[test]
+    fn read_string_without_nt() {
+        let data: Vec<u8> = vec![b'a', 0x00, b'b', 0x00, b'c', 0x00]; // abc
+        let data_slice = &mut &data[..];
+
+        let err = read_nt_utf16_string(data_slice).expect_err("Invalid string should return error");
+
+        if let StringParseError::Read(io_err) = err {
+            assert_eq!(io_err.kind(), std::io::ErrorKind::UnexpectedEof);
+        } else {
+            panic!("Error was not read error");
+        };
+    }
+
+    #[test]
+    fn read_string_invalid_utf16() {
+        let data: Vec<u8> = vec![0x00, 0xD8, 0x69, 0x00, 0x00, 0x00];
+        let data_slice = &mut &data[..];
+
+        let err = read_nt_utf16_string(data_slice).expect_err("Invalid string should return error");
+
+        if let StringParseError::Parse(_) = err {
+        } else {
+            panic!("Error was not parse error");
+        };
     }
 }
