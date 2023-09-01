dbae3276825919d2dcb9ab78e0384a41c7319754
iTrooz <hey@itrooz.fr>
2023-09-01 18:47:56 +0200

    feat(efivar): add BootVarWriter#set_boot_order()

--- efivar/src/boot/writer.rs
+++ efivar/src/boot/writer.rs
@@ -10,9 +10,25 @@ use super::BootEntry;
 
 pub trait BootVarWriter {
     fn add_boot_entry(&mut self, id: u16, entry: BootEntry) -> crate::Result<()>;
+    fn set_boot_order(&mut self, ids: Vec<u16>) -> crate::Result<()>;
 }
 
 impl<T: VarWriter> BootVarWriter for T {
+    fn set_boot_order(&mut self, ids: Vec<u16>) -> crate::Result<()> {
+        let bytes: Vec<u8> = ids.into_iter().flat_map(|id| id.to_le_bytes()).collect();
+
+        self.write(
+            &Variable::new("BootOrder"),
+            VariableFlags::NON_VOLATILE
+                | VariableFlags::BOOTSERVICE_ACCESS
+                | VariableFlags::RUNTIME_ACCESS,
+            &bytes,
+        )
+        .unwrap();
+
+        Ok(())
+    }
+
     fn add_boot_entry(&mut self, id: u16, entry: BootEntry) -> crate::Result<()> {
         let bytes = entry.to_bytes();
 
