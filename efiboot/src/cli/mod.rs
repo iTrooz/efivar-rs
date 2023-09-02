e2b2367dec293d3797838cca9462fd2befcce8e4
iTrooz <hey@itrooz.fr>
2023-09-02 15:30:45 +0200

    refactor(efiboot): move commands handling to crate::cli

--- efiboot/src/cli/mod.rs
+++ efiboot/src/cli/mod.rs
@@ -1,6 +1,116 @@
+use std::path::PathBuf;
+
+use efivar::VarManager;
+use structopt::StructOpt;
+
+use self::boot::BootCommand;
+
 pub mod boot;
 pub mod delete;
 pub mod dump;
 pub mod import;
 pub mod list;
 pub mod read;
+
+#[derive(StructOpt)]
+pub enum Command {
+    /// Read the value of a variable
+    Read {
+        /// Name of the variable to read
+        #[structopt(value_name = "VARIABLE")]
+        name: String,
+
+        /// GUID of the namespace. Default: EFI standard namespace
+        #[structopt(short, long, value_name = "NAMESPACE")]
+        namespace: Option<uuid::Uuid>,
+
+        /// Print the value as an UTF-8 string
+        #[structopt(short, long)]
+        string: bool,
+    },
+    /// List known EFI variables
+    List {
+        /// GUID of the namespace. Default: EFI standard namespace
+        #[structopt(short, long, value_name = "NAMESPACE")]
+        namespace: Option<uuid::Uuid>,
+        /// ignore --namespace and show all namespaces
+        #[structopt(short, long)]
+        all: bool,
+    },
+    /// Delete an EFI variabe
+    Delete {
+        /// Name of the variable to delete
+        #[structopt(value_name = "VARIABLE")]
+        name: String,
+
+        /// GUID of the namespace. Default: EFI standard namespace
+        #[structopt(short, long, value_name = "NAMESPACE")]
+        namespace: Option<uuid::Uuid>,
+    },
+
+    /// Manage boot-related variables
+    Boot(BootCommand),
+    /// Dump a variable to file
+    Dump {
+        /// Name of the variable to dump
+        #[structopt(value_name = "VARIABLE")]
+        name: String,
+
+        /// GUID of the namespace. Default: EFI standard namespace
+        #[structopt(short, long, value_name = "NAMESPACE")]
+        namespace: Option<uuid::Uuid>,
+
+        /// Output file
+        #[structopt(value_name = "OUTPUT_FILE")]
+        output_file: PathBuf,
+    },
+    /// Import a variable from a file
+    Import {
+        /// Input file
+        #[structopt(value_name = "INPUT_FILE")]
+        input_file: PathBuf,
+
+        /// Name of the variable to create
+        #[structopt(value_name = "VARIABLE")]
+        name: String,
+
+        /// GUID of the namespace. Default: EFI standard namespace
+        #[structopt(short, long, value_name = "NAMESPACE")]
+        namespace: Option<uuid::Uuid>,
+    },
+}
+
+pub fn run(manager: Box<dyn VarManager>, cmd: Command) {
+    match cmd {
+        Command::Read {
+            name,
+            namespace,
+            string,
+        } => {
+            read::run(manager, &name, namespace, string);
+        }
+        Command::List { namespace, all } => {
+            list::run(manager, namespace, all);
+        }
+        Command::Delete { name, namespace } => {
+            delete::run(manager, &name, namespace);
+        }
+        Command::Boot(arg) => {
+            boot::run(manager, arg);
+        }
+        Command::Dump {
+            name,
+            namespace,
+            output_file,
+        } => {
+            dump::run(manager, &name, namespace, &output_file);
+        }
+        Command::Import {
+            input_file,
+            name,
+            namespace,
+        } => {
+            import::run(manager, &input_file, &name, namespace);
+        }
+    }
+}
--- efiboot/src/main.rs
+++ efiboot/src/main.rs
@@ -1,77 +1,9 @@
 mod cli;
 pub mod id;
 
-use cli::boot::BootCommand;
+use cli::Command;
 use std::path::PathBuf;
 use structopt::StructOpt;
-
-#[derive(StructOpt)]
-enum Command {
-    /// Read the value of a variable
-    Read {
-        /// Name of the variable to read
-        #[structopt(value_name = "VARIABLE")]
-        name: String,
-
-        /// GUID of the namespace. Default: EFI standard namespace
-        #[structopt(short, long, value_name = "NAMESPACE")]
-        namespace: Option<uuid::Uuid>,
-
-        /// Print the value as an UTF-8 string
-        #[structopt(short, long)]
-        string: bool,
-    },
-    /// List known EFI variables
-    List {
-        /// GUID of the namespace. Default: EFI standard namespace
-        #[structopt(short, long, value_name = "NAMESPACE")]
-        namespace: Option<uuid::Uuid>,
-        /// ignore --namespace and show all namespaces
-        #[structopt(short, long)]
-        all: bool,
-    },
-    /// Delete an EFI variabe
-    Delete {
-        /// Name of the variable to delete
-        #[structopt(value_name = "VARIABLE")]
-        name: String,
-
-        /// GUID of the namespace. Default: EFI standard namespace
-        #[structopt(short, long, value_name = "NAMESPACE")]
-        namespace: Option<uuid::Uuid>,
-    },
-
-    /// Manage boot-related variables
-    Boot(BootCommand),
-    /// Dump a variable to file
-    Dump {
-        /// Name of the variable to dump
-        #[structopt(value_name = "VARIABLE")]
-        name: String,
-
-        /// GUID of the namespace. Default: EFI standard namespace
-        #[structopt(short, long, value_name = "NAMESPACE")]
-        namespace: Option<uuid::Uuid>,
-
-        /// Output file
-        #[structopt(value_name = "OUTPUT_FILE")]
-        output_file: PathBuf,
-    },
-    /// Import a variable from a file
-    Import {
-        /// Input file
-        #[structopt(value_name = "INPUT_FILE")]
-        input_file: PathBuf,
-
-        /// Name of the variable to create
-        #[structopt(value_name = "VARIABLE")]
-        name: String,
-
-        /// GUID of the namespace. Default: EFI standard namespace
-        #[structopt(short, long, value_name = "NAMESPACE")]
-        namespace: Option<uuid::Uuid>,
-    },
-}
 
 #[derive(StructOpt)]
 #[structopt(name = env!("CARGO_PKG_NAME"), author, about)]
@@ -98,36 +30,5 @@ fn main(opts: Opt) {
         efivar::system()
     };
 
-    match opts.cmd {
-        Command::Read {
-            name,
-            namespace,
-            string,
-        } => {
-            cli::read::run(manager, &name, namespace, string);
-        }
-        Command::List { namespace, all } => {
-            cli::list::run(manager, namespace, all);
-        }
-        Command::Delete { name, namespace } => {
-            cli::delete::run(manager, &name, namespace);
-        }
-        Command::Boot(arg) => {
-            cli::boot::run(manager, arg);
-        }
-        Command::Dump {
-            name,
-            namespace,
-            output_file,
-        } => {
-            cli::dump::run(manager, &name, namespace, &output_file);
-        }
-        Command::Import {
-            input_file,
-            name,
-            namespace,
-        } => {
-            cli::import::run(manager, &input_file, &name, namespace);
-        }
-    }
+    cli::run(manager, opts.cmd);
 }
