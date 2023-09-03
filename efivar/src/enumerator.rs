af20c59adc7714a49654f1073322991fd6d176e9
iTrooz <hey@itrooz.fr>
2023-09-03 11:59:10 +0200

    doc(efivar): remove obsolete doc comment

--- efivar/src/enumerator.rs
+++ efivar/src/enumerator.rs
@@ -3,8 +3,5 @@ use crate::efi::Variable;
 /// Represents the capability of enumerating EFI variables
 pub trait VarEnumerator {
     /// Enumerates all known variables on the system. Returns a list of found variable names.
-    ///
-    /// *Note that some implementations of `VarEnumerator` rely on a static list since the
-    /// underlying OS is not capable of enumerating variables.*
     fn get_all_vars<'a>(&'a self) -> crate::Result<Box<dyn Iterator<Item = Variable> + 'a>>;
 }
