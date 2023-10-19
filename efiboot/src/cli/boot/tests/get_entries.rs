2a3fe44a319998d51e6e6e3f5110df387efac131
iTrooz <hey@itrooz.fr>
2023-10-19 17:38:52 +0200

    test(efiboot): add more tests to `efiboot boot get-entries`

--- efiboot/src/cli/boot/tests/get_entries.rs
+++ efiboot/src/cli/boot/tests/get_entries.rs
@@ -2,7 +2,10 @@ use clap::Parser;
 use efivar::store::MemoryStore;
 
 use crate::{
-    cli::{boot::tests::standard_setup, Command},
+    cli::{
+        boot::tests::{add_entry, standard_setup},
+        Command,
+    },
     exit_code::ExitCode,
 };
 
@@ -11,6 +14,37 @@ fn get_entries() {
     let manager = &mut MemoryStore::new();
 
     standard_setup(manager, 0x0001);
+
+    assert_eq!(
+        ExitCode::SUCCESS,
+        crate::run(
+            Command::parse_from(["efiboot", "boot", "get-entries"]),
+            manager,
+        )
+    );
+}
+
+#[test]
+fn get_entries_verbose() {
+    let manager = &mut MemoryStore::new();
+
+    standard_setup(manager, 0x0001);
+
+    assert_eq!(
+        ExitCode::SUCCESS,
+        crate::run(
+            Command::parse_from(["efiboot", "boot", "get-entries", "-v"]),
+            manager,
+        )
+    );
+}
+
+#[test]
+fn get_entries_not_in_bootorder() {
+    let manager = &mut MemoryStore::new();
+
+    standard_setup(manager, 0x0001);
+    add_entry(manager, 0x0002, true);
 
     assert_eq!(
         ExitCode::SUCCESS,
