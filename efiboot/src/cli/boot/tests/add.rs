83b7c3184c3592e8919998dc4b677081ee803128
iTrooz <hey@itrooz.fr>
2023-12-23 13:36:35 +0100

    test(efiboot): add test for fix_file_path()

--- efiboot/src/cli/boot/tests/add.rs
+++ efiboot/src/cli/boot/tests/add.rs
@@ -115,6 +115,59 @@ fn add_set_id() {
 }
 
 #[test]
+fn add_verify_file_path_fix() {
+    //! Use `efiboot boot add` with a given entry ID.
+
+    let manager = &mut MemoryStore::new();
+
+    let setup_entry = standard_setup(manager, 0x0001);
+
+    // execute `efiboot boot add`
+    assert_eq!(
+        ExitCode::SUCCESS,
+        crate::run(
+            Command::parse_from([
+                "efiboot",
+                "boot",
+                "add",
+                "--file",
+                "a/b/c",
+                "--description",
+                "Some entry",
+                "--id",
+                "1000"
+            ]),
+            manager,
+        )
+    );
+
+    // verify Boot0000 did not get inserted
+    assert_var_not_found(manager, &Variable::new("Boot0000"));
+
+    // verify inserted entry is right
+    let (data, _) = manager.read(&Variable::new("Boot1000")).unwrap();
+    let entry = BootEntry::parse(data).unwrap();
+    assert_eq!(
+        entry,
+        BootEntry {
+            attributes: BootEntryAttributes::LOAD_OPTION_ACTIVE,
+            description: "Some entry".to_owned(),
+            file_path_list: Some(FilePathList {
+                file_path: FilePath {
+                    path: "\\a\\b\\c".into()
+                },
+                hard_drive: setup_entry.file_path_list.unwrap().hard_drive // use partition defined earlier
+            }),
+            optional_data: vec![]
+        }
+    );
+
+    // verify new boot order is right
+    let (data, _) = manager.read(&Variable::new("BootOrder")).unwrap();
+    assert_eq!(data, utils::u16_to_u8(&[0x1000, 0x0001]));
+}
+
+#[test]
 fn add_on_existing() {
     //! Try to add a boot entry with an already-existing id
 
