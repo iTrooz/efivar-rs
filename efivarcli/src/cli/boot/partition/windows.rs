92b8fa0a4b659c17639c0f29fb74acd1e0b4f2e8
iTrooz <hey@itrooz.fr>
2025-06-26 14:27:51 +0200

    fix

--- efivarcli/src/cli/boot/partition/windows.rs
+++ efivarcli/src/cli/boot/partition/windows.rs
@@ -1,6 +1,8 @@
 use efivar::boot::EFIHardDrive;
 use std::path::PathBuf;
 
+pub type Partition = String;
+
 pub fn query_partition(disk: Option<String>, partition: String) -> anyhow::Result<Partition> {
     todo!();
 }
